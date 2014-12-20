87499894c274965630fe9cf57a3bf422ad234f4f
Alistair MacDonald <amacdonald@aisquared.com>
2014-12-20 02:04:10 -0500

    First commit.

---
+++ index.js
@@ -0,0 +1,52 @@
+var getJSON = require('get-json')
+  , fs = require('fs')
+  ;
+
+var fetch = {
+  market: 'bitstampUSD',
+  from: { year:2011, month:9, day:13 },
+  to: { year:2014, month:12, day:20 },
+  dataSourceURL: 'http://bitcoincharts.com/charts/chart.json?'+'m='+'bitstampUSD'+'&r=1&i=1-min&e'
+}
+
+function url(baseurl, date){
+  return baseurl + date;
+}
+
+Date.prototype.addDays = function(days) {
+  var dat = new Date(this.valueOf())
+  dat.setDate(dat.getDate() + days);
+  return dat;
+}
+
+function getDates(startDate, stopDate) {
+  var dateArray = new Array();
+  var currentDate = startDate;
+  while (currentDate <= stopDate) {
+    dateArray.push(currentDate)
+    currentDate = currentDate.addDays(1);
+  }
+  return dateArray;
+}
+
+var start = new Date(fetch.from.year, fetch.from.month-1, fetch.from.day)
+  , end = new Date(fetch.to.year, fetch.to.month-1, fetch.to.day)
+  , dates = getDates(start, end)
+  ;
+
+dates.forEach(function(date){
+  var prettyDate = date.getFullYear() +'-'+ (date.getMonth()+1) +'-'+ date.getUTCDate()
+  , dataPath = fetch.dataSourceURL + prettyDate
+  ;
+  // console.log(dataPath);
+
+  getJSON(dataPath, function(error, response){
+    fs.writeFile('data/'+fetch.market+'-'+prettyDate+'.json', JSON.stringify(response), 'utf8', function(err) {
+      if(err) {
+        console.log(err);
+      } else {
+        console.log(prettyDate + ' was saved!');
+      }
+    }); 
+  });
+});
